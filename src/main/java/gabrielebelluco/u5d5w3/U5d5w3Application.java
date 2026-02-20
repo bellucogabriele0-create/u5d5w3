785bd7c9f3c16cc90711205db9840ad72d1c0de0
bellucogabriele0-create <bellucogabriele0@gmail.com>
2026-02-20 09:06:43 +0100

    Initial commit

---
+++ src/main/java/gabrielebelluco/u5d5w3/U5d5w3Application.java
@@ -0,0 +1,13 @@
+package gabrielebelluco.u5d5w3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5d5w3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5d5w3Application.class, args);
+	}
+
+}
---
+++ src/test/java/gabrielebelluco/u5d5w3/U5d5w3ApplicationTests.java
@@ -0,0 +1,13 @@
+package gabrielebelluco.u5d5w3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5d5w3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
