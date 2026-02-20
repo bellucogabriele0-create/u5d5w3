059a29932945800d49443cb72bb0e876e2908328
bellucogabriele0-create <bellucogabriele0@gmail.com>
2026-02-20 16:50:37 +0100

    AGGIUNTA IMMAGINI
    AGGIUNTA DEGLI SCREENSHOT DI TUTTI I PASSAGGI NECESSARI DB E POSTMAN

--- src/main/java/gabrielebelluco/u5d5w3/U5d5w3Application.java
+++ src/main/java/gabrielebelluco/u5d5w3/U5d5w3Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class U5d5w3Application {
+public class U5d5w3Application { //eyJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3NzE2MDE0OTMsImV4cCI6MTc3MjIwNjI5Mywic3ViIjoiNzQ0YWRjNjYtNTM0NS00NDcyLTgwMGQtNTIzODE2ODE0M2NjIn0.PnmIUg2jij-s4pOJPEhi4IWpCNU8QwvyQeQ0TGa88TU
 
-	public static void main(String[] args) {
-		SpringApplication.run(U5d5w3Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(U5d5w3Application.class, args);
+    }
 
 }
